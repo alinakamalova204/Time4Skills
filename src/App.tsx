6da53d3410cb8b3b6b5d60ede3b86568132c2aa9
alinakamalova204 <alinakamalova919@gmail.com>
2024-07-07 20:52:17 +0300

    routing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import Chat from "./pages/Chat";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+     <header>
+         <Link to="/">Home</Link>
+         <Link to="/profile">Profile</Link>
+         <Link to="/search">Search</Link>
+         <Link to="/chat">Chat</Link>
+     </header>
+      <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/search" element={<Search/>} />
+              <Route path="/chat" element={<Chat/>} />
+      </Routes>
+        </>
   )
 }
 
---
+++ src/pages/Chat/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Chat: React.FC = () => {
+    return <h1>chat</h1>
+}
+export default Chat
